b54dd2ceaa9ac42ca87f7199eeb578d743a84e09
takuaraki <takuaraki7@gmail.com>
2016-01-30 17:05:27 +0900

    implement extractRepositories()

--- src/main/java/models/RepositoryModel.java
+++ src/main/java/models/RepositoryModel.java
@@ -6,6 +6,8 @@ import entities.repositories.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Model of Maven repository
@@ -25,8 +27,31 @@ public class RepositoryModel {
         return repositories;
     }
 
-    public List<Repository> extractRepositories(String gradleScript) {
-        // TODO not implemented yet
-        return new ArrayList<Repository>();
+    /**
+     * extract Repositories from gradle script.
+     *
+     * @param gradleScript
+     * @return
+     */
+    private List<Repository> extractRepositories(String gradleScript) {
+        String text = new String(gradleScript);
+        String regex = "maven(?: |\\n|\\t)*\\{(?: |\\n|\\t)*url(?: |\\n|\\t)*(?:'|\")(.*)(?:'|\")";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(text);
+        List<Repository> repositories = new ArrayList<Repository>();
+
+        while (m.find()) {
+            String url = m.group(1);
+            if (url.substring(url.length() - 1, url.length()).equals("/") == false) {
+                url += "/";
+            }
+            Repository repository = new Repository(url);
+            repositories.add(repository);
+
+            text = text.substring(m.end());
+            m = p.matcher(text);
+        }
+
+        return repositories;
     }
 }
