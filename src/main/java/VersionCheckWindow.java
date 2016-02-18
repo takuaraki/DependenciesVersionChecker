fc62698e1d335e3704c888a5928c77ef49a9b85c
takuaraki <takuaraki7@gmail.com>
2016-02-18 23:50:33 +0900

    remove unused codes.

--- src/main/java/VersionCheckWindow.java
+++ src/main/java/VersionCheckWindow.java
@@ -10,29 +10,21 @@ import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import entities.Dependency;
 import entities.DependencyUpdatesResult;
-import entities.Library;
-import models.LibraryModel;
-import org.apache.http.util.TextUtils;
 import org.gradle.tooling.GradleConnectionException;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
 import org.gradle.tooling.ResultHandler;
 import org.jetbrains.annotations.NotNull;
-import rx.functions.Action1;
-import rx.schedulers.Schedulers;
-import rx.schedulers.SwingScheduler;
-import viewmodels.VersionCheckViewModel;
 
 import javax.swing.*;
 import javax.swing.event.HyperlinkEvent;
-import java.awt.Desktop;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.io.*;
 import java.net.URISyntaxException;
-import java.util.List;
 
 /**
  * バーションチェックを行うToolWindow
@@ -49,12 +41,8 @@ public class VersionCheckWindow implements ToolWindowFactory {
 
     private Project currentProject;
 
-    VersionCheckViewModel versionCheckViewModel;
-
 
     public VersionCheckWindow() {
-        versionCheckViewModel = new VersionCheckViewModel();
-
         inputArea.setText(INPUT_AREA_HINT);
         inputArea.addFocusListener(new FocusListener() {
             @Override
@@ -187,33 +175,6 @@ public class VersionCheckWindow implements ToolWindowFactory {
                     .append("</tr>");
         }
 
-        stringBuilder.append("</table>");
-        return stringBuilder.toString();
-    }
-
-    private String createResult(List<Library> usingLibraries, List<Library> latestLibraries) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("<table>")
-                .append("<tr><th align=\"left\">Library</th><th align=\"left\">Using version</th><th align=\"left\">Latest version</th></tr>");
-        for (int i = 0; i < usingLibraries.size(); i++) {
-            Library usingLibrary = usingLibraries.get(i);
-
-            String extractedLibrary;
-            if (TextUtils.isEmpty(usingLibrary.getMetaDataUrl())) {
-                extractedLibrary = usingLibrary.getGroupId() + ":" + usingLibrary.getArtifactId();
-            } else {
-                extractedLibrary =
-                        "<a href=\"" + usingLibrary.getMetaDataUrl() + "\">"
-                                + usingLibrary.getGroupId() + ":" + usingLibrary.getArtifactId() + "</a>";
-            }
-
-            stringBuilder
-                    .append("<tr>")
-                    .append("<td>").append(extractedLibrary).append("</td>")
-                    .append("<td>").append(usingLibrary.getVersion()).append("</td>")
-                    .append("<td>").append(latestLibraries.get(i).getVersion()).append("</td>")
-                    .append("</tr>");
-        }
         stringBuilder.append("</table>");
         return stringBuilder.toString();
     }
